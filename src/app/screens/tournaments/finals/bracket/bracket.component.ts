917ae4c1be682b5d525498802922903b32699e1f
laviida <sergi1996cn@gmail.com>
2021-02-24 15:15:16 +0100

    arreglat

--- src/app/screens/tournaments/finals/bracket/bracket.component.ts
+++ src/app/screens/tournaments/finals/bracket/bracket.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Match } from 'src/app/models/match';
 
 @Component({
-  selector: 'app-bracket',
+  selector: '[app-bracket]',
   templateUrl: './bracket.component.html',
   styleUrls: ['./bracket.component.scss'],
 })
